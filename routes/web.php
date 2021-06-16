0cef040da79a28b13ac40c80d22e5dcf4c29323d
Mahbubul Hasan <mahbubul161@gmail.com>
2021-06-16 22:56:46 +0600

    Create user seeder file

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Route::namespace('Front')->group(function(){
     Route::get('/', 'HomeController@index')->name('/');
@@ -84,6 +76,6 @@ Route::namespace('Admin')->prefix('admin')->as('admin.')->middleware('auth')->gr
 
 Auth::routes();
 
-Route::fallback(function(){
-    return view('front.404.404');
-});
+// Route::fallback(function(){
+//     return view('front.404.404');
+// });
